7951c3794dcaf6ab11a1f9148bfc00e10a0d6bcb
AmrAlaraby <amr2amr3@gmail.com>
2025-02-23 06:14:45 +0200

    dn

--- src/components/Verifycode/Verifycode.tsx
+++ src/components/Verifycode/Verifycode.tsx
@@ -72,7 +72,7 @@ const navigate = useNavigate()
    <form onSubmit={formik.handleSubmit} className=" mx-auto">
   
   <div className="relative z-0 w-full mb-5 group">
-    <input onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.resetCode} type="text" name="resetCode" id="resetCode" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" "  />
+    <input onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.resetCode} type="text" name="resetCode" id="resetCode" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-600 dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" "  />
     <label htmlFor="resetCode" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">verification code :</label>
   </div>
 
