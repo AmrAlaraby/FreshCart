6744ae1f632bd48786427a89a1ea49467d214e1b
AmrAlaraby <amr2amr3@gmail.com>
2025-02-23 05:36:09 +0200

    doneee

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -137,13 +137,13 @@ export default function ProductDetails() {
                 <div className="flex justify-between items-center">
                 <p>Price: ${product.price}</p>
                 <p><i className='fas fa-star text-yellow-400'></i>{product.ratingsAverage}</p>
-                </div>
+                </div></Link>
                 <div className="w-full flex justify-center">
                   <div className="btn">
                   <button className="text-white  bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Add to cart</button>
                   </div>
                 </div>
-                  </Link>
+                  
               </div>
               </>
             })
